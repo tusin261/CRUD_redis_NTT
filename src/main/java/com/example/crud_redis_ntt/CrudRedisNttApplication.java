0bb5c959c83eb349d73e1851408cc6cbe35bbed4
tusin26 <thanhtu3885585@gmail.com>
2021-11-05 07:48:35 +0700

    set up

---
+++ src/main/java/com/example/crud_redis_ntt/CrudRedisNttApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_ntt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRedisNttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisNttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_redis_ntt/CrudRedisNttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_ntt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisNttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
