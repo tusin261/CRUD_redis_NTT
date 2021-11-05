82ef3559f042d207ecc03efa9a435d7bca0d1605
tusin26 <thanhtu3885585@gmail.com>
2021-11-05 08:03:56 +0700

    finish hashopenrations

--- src/main/java/com/example/crud_redis_ntt/CrudRedisNttApplication.java
+++ src/main/java/com/example/crud_redis_ntt/CrudRedisNttApplication.java
@@ -2,6 +2,9 @@ package com.example.crud_redis_ntt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class CrudRedisNttApplication {
@@ -10,4 +13,15 @@ public class CrudRedisNttApplication {
         SpringApplication.run(CrudRedisNttApplication.class, args);
     }
 
+    @Bean
+    LettuceConnectionFactory jedisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
 }
